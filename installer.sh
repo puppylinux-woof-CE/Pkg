3e82ce630121b9c361d2ecec1af8f608649326ab
sc0ttj <scottj@uk2.net>
2018-03-15 23:20:02 +0800

    added installer.sh: - copy files to / - add list of copied files to ~/.packages/*.files

---
+++ installer.sh
@@ -0,0 +1,11 @@
+#!/bin/ash
+
+pkgname=pkg-1.9.19alpha-noarch
+
+cp -r etc/ root/ sbin/ usr/ / && echo "Pkg installed OK" || echo "Pkg was NOT installed!"
+
+find etc/ root/ sbin/ usr/ | sed -e 's/^/\//g' > ~/.packages/${pkgname}.files
+
+[ ! -z ~/.packages/${pkgname}.files ] && echo "Package contents listed in ~/.packages/${pkgname}.files"
+
+exit 0
