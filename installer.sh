d8899d2136f9d9d121aca52aec23a64231d82098
sc0ttj <scottj@uk2.net>
2018-11-29 22:29:18 +0800

    installer improvements: dont overwrite existing repos lists: - prevents third-party (user-added) repos from being removed by installer

--- installer.sh
+++ installer.sh
@@ -2,9 +2,25 @@
 
 pkgname=pkg-1.9.x-noarch
 
-cp -r etc/ root/ sbin/ usr/ / && echo -e "Pkg installed OK \n" || echo -e "Pkg was NOT installed! \n"
+mkdir -p /tmp/pkg
 
+# dont overwrite the existing ~/.pkg/sources[-all] files, if they exist
+for existing_file in /root/.pkg/sources /root/.pkg/sources-all
+do
+  [ -f ".$existing_file" ] && mv ".$existing_file" /tmp/pkg/
+done
+
+# copy over our new Pkg files
+mv /usr/sbin/pkg /usr/sbin/pkg.previous
+cp -r etc/ root/ sbin/ usr/ / && echo -e "Pkg installed OK \n" || echo -e "Pkg was NOT installed! \n"
+# list all Pkg files (not including sources files, cos user might want to keep their added repos)
 find etc/ root/ sbin/ usr/ | sed -e 's/^/\//g' > ~/.packages/${pkgname}.files
+
+# put the files back again
+for existing_file in /tmp/pkg/sources /tmp/pkg/sources-all
+do
+  mv $existing_file ./root/.pkg/
+done
 
 [ ! -z ~/.packages/${pkgname}.files ] && echo -e "Package contents listed in ~/.packages/${pkgname}.files \n"
 
@@ -20,7 +36,7 @@ pkg update-sources && echo && pkg repo-update && \
 
   echo
   echo 'For a basic intro, use `pkg welcome`'
-  echo 'For more help, use `pkg help` or `pkg help-all`'
+  echo 'For more help, use `pkg help`, `pkg help-all` or `pkg usage`'
   echo
 }
 
