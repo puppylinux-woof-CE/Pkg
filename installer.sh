b2d8916368f6bed261dec645f9895259ae05b7c0
sc0ttj <scottj@uk2.net>
2019-01-24 21:57:01 +0800

    updates to installer - nicer output

--- installer.sh
+++ installer.sh
@@ -2,35 +2,43 @@
 
 pkgname=pkg-1.9.x-noarch
 
-mkdir -p /tmp/pkg
+mkdir -p /tmp/pkg 2>/dev/null
 
 # dont overwrite the existing ~/.pkg/sources[-all] files, if they exist
-for existing_file in /root/.pkg/sources /root/.pkg/sources-all
+
+for existing_file in /root/.pkg/sources /root/.pkg/sources-all /root/.pkg/sources-user
 do
-  [ -f ".$existing_file" ] && mv ".$existing_file" /tmp/pkg/
+  [ -f "$existing_file" ] && mv "$existing_file" /tmp/pkg/
 done
 
 # copy over our new Pkg files
-mv /usr/sbin/pkg /usr/sbin/pkg.previous
+
+mv /usr/sbin/pkg /usr/sbin/pkg.previous 2>/dev/null
+
 cp -r etc/ root/ sbin/ usr/ / && echo -e "Pkg installed OK \n" || echo -e "Pkg was NOT installed! \n"
+
 # list all Pkg files (not including sources files, cos user might want to keep their added repos)
 find etc/ root/ sbin/ usr/ | sed -e 's/^/\//g' > ~/.packages/${pkgname}.files
 
 # put the files back again
-for existing_file in /tmp/pkg/sources /tmp/pkg/sources-all
+for existing_file in /tmp/pkg/sources /tmp/pkg/sources-all /tmp/pkg/sources-user
 do
-  mv $existing_file ./root/.pkg/
+  [ -f $existing_file ] && mv $existing_file /root/.pkg/
 done
 
 [ ! -z ~/.packages/${pkgname}.files ] && echo -e "Package contents listed in ~/.packages/${pkgname}.files \n"
 
 echo -e "Setting up Pkg... \n"
 
+pkg update-sources &>/dev/null
+
+echo
 pkg welcome
 echo
 
-pkg update-sources && echo && pkg repo-update && \
+pkg repo-update && \
 {
+  sleep 0.1
   echo -e "\nAvailable repos: \n"
   pkg repo-list
 
